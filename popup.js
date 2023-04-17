c85439d2f92b26f77e15815e8a6529530e7e1e44
Marc <mkarim117@berkeley.edu>
2023-04-16 17:22:25 -0700

    script load

--- popup.js
+++ popup.js
@@ -16,3 +16,28 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const summarizeButton = document.getElementById("summarize");
+  if (summarizeButton) {
+    summarizeButton.addEventListener("click", () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "summarize_tos" });
+      });
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "display_summary") {
+    const summaryList = document.getElementById("summary");
+    summaryList.innerHTML = "";
+
+    request.summary.forEach((point) => {
+      const li = document.createElement("li");
+      li.textContent = point;
+      summaryList.appendChild(li);
+    });
+  }
+});
