729b2788ed583f46871f5dffc098b833d6a6bfad
Marc <mkarim117@berkeley.edu>
2023-04-16 17:16:17 -0700

    summarizeIt Functionality

--- content.js
+++ content.js
@@ -1,2 +1,21 @@
-// This script can interact with the current page's DOM
-// It is not necessary for this example, but you can use it if needed
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "summarize_tos") {
+      const tosElement = document.querySelector("YOUR_TOS_SELECTOR"); // Replace "YOUR_TOS_SELECTOR" with a suitable selector for the target website
+      if (tosElement) {
+        const tosText = tosElement.textContent;
+  
+        chrome.runtime.sendMessage(
+          { action: "fetch_summary", text: tosText },
+          (response) => {
+            chrome.runtime.sendMessage({
+              action: "display_summary",
+              summary: response.summary,
+            });
+          }
+        );
+      } else {
+        alert("No Terms of Service found on this page.");
+      }
+    }
+  });
+  
