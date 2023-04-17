184cf34ff6ae8496495bd8a8412e5c335d2cc1de
Marc <mkarim117@berkeley.edu>
2023-04-16 17:41:30 -0700

    revert

--- content.js
+++ content.js
@@ -1,26 +1,48 @@
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "summarize_tos") {
       const tosElements = findTosElements();
       if (tosElements.length > 0) {
         const tosText = extractTosText(tosElements);
-        const summary = await fetchSummary(tosText);
-        sendResponse({ summary: summary });
+  
+        chrome.runtime.sendMessage(
+          { action: "fetch_summary", text: tosText },
+          (response) => {
+            if (response && response.summary) {
+              chrome.runtime.sendMessage({
+                action: "display_summary",
+                summary: response.summary,
+              });
+            }
+          }
+        );
       } else {
         alert("No Terms of Service found on this page.");
       }
     }
-    return true; // Required to handle asynchronous responses.
   });
   
   function findTosElements() {
-    // ...
+    const keywords = ["terms of service", "terms and conditions"];
+    const allElements = Array.from(document.querySelectorAll("body *"));
+    const tosElements = [];
+  
+    allElements.forEach((element) => {
+      const textContent = element.textContent.toLowerCase();
+  
+      keywords.forEach((keyword) => {
+        if (textContent.includes(keyword)) {
+          tosElements.push(element);
+        }
+      });
+    });
+  
+    return tosElements;
   }
   
   function extractTosText(tosElements) {
-    // ...
-  }
-  
-  async function fetchSummary(text) {
-    // ...
+    return tosElements
+      .map((element) => element.textContent)
+      .join("\n\n")
+      .trim();
   }
   
