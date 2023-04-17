535c1236753d080f320c072ab1d492a19c555f09
Marc <mkarim117@berkeley.edu>
2023-04-16 17:33:56 -0700

    nfaf

--- content.js
+++ content.js
@@ -1,48 +1,26 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (request.action === "summarize_tos") {
       const tosElements = findTosElements();
       if (tosElements.length > 0) {
         const tosText = extractTosText(tosElements);
-  
-        chrome.runtime.sendMessage(
-          { action: "fetch_summary", text: tosText },
-          (response) => {
-            if (response && response.summary) {
-              chrome.runtime.sendMessage({
-                action: "display_summary",
-                summary: response.summary,
-              });
-            }
-          }
-        );
+        const summary = await fetchSummary(tosText);
+        sendResponse({ summary: summary });
       } else {
         alert("No Terms of Service found on this page.");
       }
     }
+    return true; // Required to handle asynchronous responses.
   });
   
   function findTosElements() {
-    const keywords = ["terms of service", "terms and conditions"];
-    const allElements = Array.from(document.querySelectorAll("body *"));
-    const tosElements = [];
-  
-    allElements.forEach((element) => {
-      const textContent = element.textContent.toLowerCase();
-  
-      keywords.forEach((keyword) => {
-        if (textContent.includes(keyword)) {
-          tosElements.push(element);
-        }
-      });
-    });
-  
-    return tosElements;
+    // ...
   }
   
   function extractTosText(tosElements) {
-    return tosElements
-      .map((element) => element.textContent)
-      .join("\n\n")
-      .trim();
+    // ...
+  }
+  
+  async function fetchSummary(text) {
+    // ...
   }
   
--- popup.js
+++ popup.js
@@ -1,30 +1,16 @@
 document.getElementById("summarize").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "summarize_tos" });
+    chrome.tabs.sendMessage(tabs[0].id, { action: "summarize_tos" }, (response) => {
+      if (response && response.summary) {
+        const summaryList = document.getElementById("summary");
+        summaryList.innerHTML = "";
+
+        response.summary.forEach((point) => {
+          const li = document.createElement("li");
+          li.textContent = point;
+          summaryList.appendChild(li);
+        });
+      }
+    });
   });
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "display_summary") {
-    const summaryList = document.getElementById("summary");
-    summaryList.innerHTML = "";
-
-    request.summary.forEach((point) => {
-      const li = document.createElement("li");
-      li.textContent = point;
-      summaryList.appendChild(li);
-    });
-  }
-});
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const summarizeButton = document.getElementById("summarize");
-  if (summarizeButton) {
-    summarizeButton.addEventListener("click", () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "valleyProject" });
-      });
-    });
-  }
 });
