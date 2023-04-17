a37e62f98033962462b499963b80f55a46127b41
Marc <mkarim117@berkeley.edu>
2023-04-16 17:17:40 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "summarize_tos") {
-      const tosElement = document.querySelector("YOUR_TOS_SELECTOR"); // Replace "YOUR_TOS_SELECTOR" with a suitable selector for the target website
-      if (tosElement) {
-        const tosText = tosElement.textContent;
+      const tosElements = findTosElements();
+      if (tosElements.length > 0) {
+        const tosText = extractTosText(tosElements);
   
         chrome.runtime.sendMessage(
           { action: "fetch_summary", text: tosText },
@@ -18,4 +18,29 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
     }
   });
+  
+  function findTosElements() {
+    const keywords = ["terms of service", "terms and conditions"];
+    const allElements = Array.from(document.querySelectorAll("body *"));
+    const tosElements = [];
+  
+    allElements.forEach((element) => {
+      const textContent = element.textContent.toLowerCase();
+  
+      keywords.forEach((keyword) => {
+        if (textContent.includes(keyword)) {
+          tosElements.push(element);
+        }
+      });
+    });
+  
+    return tosElements;
+  }
+  
+  function extractTosText(tosElements) {
+    return tosElements
+      .map((element) => element.textContent)
+      .join("\n\n")
+      .trim();
+  }
   
